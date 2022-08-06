bcd7f7cab39af2d3beac284ee60fcc7371952019
Raghav Bajaj <raghav.bajaj@ukg.com>
2022-08-06 19:40:11 +0530

    add it

---
+++ back_up/src/app/app.module.ts
@@ -0,0 +1,31 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { CustomerComponent } from './customer/customer.component';
+import { HttpClientModule } from '@angular/common/http';
+import { CreateCustomerComponent } from './create-customer/create-customer.component';
+import { FormsModule } from '@angular/forms';
+import { UpdateCustomerComponent } from './update-customer/update-customer.component';
+import { CustomerDetailsComponent } from './customer-details/customer-details.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    CustomerComponent,
+    CreateCustomerComponent,
+    UpdateCustomerComponent,
+    CustomerDetailsComponent,
+    PageNotFoundComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule, HttpClientModule, FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ back_up/src/app/customer.ts
@@ -0,0 +1,8 @@
+export class Customer {
+    cust_id! : number;
+    cust_name!: string;
+    cust_gender!: string;
+    cust_martial_status!:string;
+	cust_address!:string;
+    cust_image!: string;
+}
